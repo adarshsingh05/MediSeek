dcbd57188d3494003b903a6d5cb98e877dc69773
adarsh singh <adarsh.22bce7232@vitapstudent.ac.in>
2025-02-13 19:34:24 +0530

    same changes

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -48,7 +48,7 @@ const LandingPage = () => {
     mediseek.<span className="text-gray-500">ai</span>
   </div>
   <div className="hidden text-xl font-extrabold cursor-pointer md:flex font-mono space-x-6 text-[#00d5be]">
-    {["Home", "Enterprise", "Products", "About", "Blog", "Careers"].map((item) => (
+    {["Home", "Your Reports", "Dashboard", "About", "Features"].map((item) => (
       <li 
         key={item}
         href="#"
@@ -59,7 +59,7 @@ const LandingPage = () => {
     ))}
   </div>
   <button className="bg-teal-400 text-black px-4 py-2 rounded-lg hover:bg-teal-500">
-    Free Trial
+    Get In ⮞
   </button>
 </nav>
 
